78249162bd5c083c1c07a80b593c9081c1ed9178
Luciano Bohnert <luciano.bohnert@gmail.com>
2017-09-04 15:05:38 -0300

    Configurando Log4j

--- src/main/java/br/com/caelum/camel/RotaRedmine.java
+++ src/main/java/br/com/caelum/camel/RotaRedmine.java
@@ -4,16 +4,23 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class RotaRedmine {
+	private static final Logger log = LogManager.getLogger(RotaRedmine.class);
 
 	public static void main(String[] args) throws Exception {
 		String password = args[0];
 
+		log.info("Iniciando RotaRedmine");
+		
 		CamelContext context = new DefaultCamelContext();
 		context.addRoutes(new RouteBuilder() {
+			
 			@Override
 			public void configure() throws Exception {
+				log.info("Início da configuração da rota");
 				/* inicia com timer */
 				from("timer://timer-qualquer?fixedRate=true&delay=1s&period=360s").id("from-timer").
 				/* dar nome a rota */
@@ -28,10 +35,14 @@ public class RotaRedmine {
 				setHeader(Exchange.FILE_NAME, constant("issues.xml")).
 				/* finaliza */
 				to("file:saida");
+				log.info("Fim da Configuração da Rota");
 			}
 		});
-
+		log.info("Iniciando Contexto Camel");
 		context.start(); // aqui camel realmente começa a trabalhar
+		long sleepTime = 20000;
+		log.info("Pausando Thread Main por {} segundos", sleepTime);
 		Thread.sleep(20000); // esperando um pouco para dar um tempo para camel
+		log.info("Fim do Rota Redmine");
 	}
 }
